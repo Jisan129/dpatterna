7d647ce37ec963a32a5edc4f1336dbc1c8c0ed2b
Jisan <jishananam@iut-dhaka.edu>
2021-08-21 12:46:12 +0600

    Decorator Pattern Implementation

---
+++ src/patterns/decorator/starbuzz-decorator.ts
@@ -0,0 +1,54 @@
+interface StarbuzzComponent {
+    getName(): string
+
+    getCost(): number
+}
+class Espresso implements StarbuzzComponent{
+    getCost(): number {
+        return 10;
+    }
+
+    getName(): string {
+        return "Espresso ";
+    }
+
+}
+abstract class StarbuzzDecorator implements StarbuzzComponent {
+    component:StarbuzzComponent
+    constructor(starbuzz:StarbuzzComponent) {
+    this.component = starbuzz
+    }
+     getCost():number{
+        return this.component.getCost()
+     }
+
+    getName():string{
+        return this.component.getName()
+    }
+
+}
+
+class whipComponent extends StarbuzzDecorator {
+    constructor(starbuzz:StarbuzzComponent){
+        super(starbuzz)
+    }
+
+    getCost(): number {
+        return 5 + super.getCost();
+    }
+
+    getName(): string {
+        return `${super.getName()} with WhipComponent`;
+    }
+
+}
+const coffee = new Espresso();
+coffee.getName()
+coffee.getCost()
+
+const newCoffee = new whipComponent(coffee)
+newCoffee.getCost()
+
+/*
+const superCoffee =new whipComponent(newCoffee)
+superCoffee.getCost()*/
